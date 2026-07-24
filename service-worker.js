09408b6668c3fb22156f2165fb0794e518647769
lucasmansillaokk1 <lucasmansillaokk2@gmail.com>
2026-07-23 22:00:52 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'finesse-pos-v1'
+const CACHE_NAME = 'finesse-pos-v3'
 const urlsToCache = [
   '/',
   '/index.html',
@@ -7,13 +7,23 @@ const urlsToCache = [
 ]
 
 self.addEventListener('install', event => {
+  self.skipWaiting()
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   )
 })
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  )
+  self.clients.claim()
+})
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    fetch(event.request).catch(() => caches.match(event.request))
   )
 })
